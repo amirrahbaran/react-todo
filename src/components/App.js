74bf93c09a796c9087097b7270f3dabd3fd27f77
AmirrezaRahbaran <amirrezarahbaran@gmail.com>
2019-06-11 15:48:27 +0430

    Apps and todoitems reformatted

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import TodosRemaining from './TodosRemaining';
-import TodoItem from './TodoItem';
-import TodosCheckAll from './TodosCheckAll';
-import TodosFiltered from './TodosFiltered';
-import TodosClearCompleted from './TodosClearCompleted';
-import { inject, observer } from 'mobx-react';
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import "../App.css";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import TodosRemaining from "./TodosRemaining";
+import TodoItem from "./TodoItem";
+import TodosCheckAll from "./TodosCheckAll";
+import TodosFiltered from "./TodosFiltered";
+import TodosClearCompleted from "./TodosClearCompleted";
+import { inject, observer } from "mobx-react";
 
-@inject('TodoStore')
+@inject("TodoStore")
 @observer
 class App extends Component {
   render() {
@@ -21,16 +21,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <div className="Todo-container">
-          <input type="text" className="todo-input" placeholder="What needs to be done" ref={TodoStore.todoInput} onKeyUp={TodoStore.addTodo} />
+          <input
+            type="text"
+            className="todo-input"
+            placeholder="What needs to be done"
+            ref={TodoStore.todoInput}
+            onKeyUp={TodoStore.addTodo}
+          />
 
           <ReactCSSTransitionGroup
             transitionName="fade"
             transitionEnterTimeout={300}
             transitionLeaveTimeout={300}
           >
-          {TodoStore.todosFiltered.map(todo =>
-            <TodoItem key={todo.id} todo={todo} />
-          )}
+            {TodoStore.todosFiltered.map(todo => (
+              <TodoItem key={todo.id} todo={todo} />
+            ))}
           </ReactCSSTransitionGroup>
 
           <div className="extra-container">
@@ -46,14 +52,11 @@ class App extends Component {
               transitionEnterTimeout={300}
               transitionLeaveTimeout={300}
             >
-            {TodoStore.todosCompletedCount > 0 &&
-              <TodosClearCompleted />
-            }
+              {TodoStore.todosCompletedCount > 0 && <TodosClearCompleted />}
             </ReactCSSTransitionGroup>
-
           </div>
-
-        </div> { /* End Todo-Container */ }
+        </div>{" "}
+        {/* End Todo-Container */}
       </div>
     );
   }
